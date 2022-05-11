dae1d3672b94d5833b45b1edc5fbe38988ab63a6
andrewDubyk <andrew.dubyk7@gmail.com>
2022-05-11 20:35:54 +0300

    Add few more tests for add_numbers algorithm

--- tests/add_numbers/mod.rs
+++ tests/add_numbers/mod.rs
@@ -5,22 +5,47 @@ use std::collections::LinkedList;
 
 #[test]
 fn it_adds_two_lists() {
-    let mut l1 = LinkedList::new();
-    l1.push_back(7);
-    l1.push_back(2);
-    l1.push_back(4);
-    l1.push_back(3);
+    {
+        let mut l1 = LinkedList::new();
+        l1.push_back(7);
+        l1.push_back(2);
+        l1.push_back(4);
+        l1.push_back(3);
 
-    let mut l2 = LinkedList::new();
-    l2.push_back(5);
-    l2.push_back(6);
-    l2.push_back(4);
+        let mut l2 = LinkedList::new();
+        l2.push_back(5);
+        l2.push_back(6);
+        l2.push_back(4);
 
-    let mut result = LinkedList::new();
-    result.push_back(7);
-    result.push_back(8);
-    result.push_back(0);
-    result.push_back(7);
+        let mut result = LinkedList::new();
+        result.push_back(7);
+        result.push_back(8);
+        result.push_back(0);
+        result.push_back(7);
 
-    assert_eq!(result, add_two_lists(l1, l2));
+        assert_eq!(result, add_two_lists(l1, l2));
+    }
+    {
+        let mut l1 = LinkedList::new();
+        l1.push_back(7);
+        l1.push_back(0);
+        l1.push_back(9);
+        l1.push_back(9);
+
+        let mut l2 = LinkedList::new();
+        l2.push_back(5);
+        l2.push_back(6);
+        l2.push_back(4);
+        l2.push_back(1);
+        l2.push_back(8);
+
+        let mut result = LinkedList::new();
+        result.push_back(6);
+        result.push_back(3);
+        result.push_back(5);
+        result.push_back(1);
+        result.push_back(7);
+
+        assert_eq!(result, add_two_lists(l1, l2));
+    }
 }
