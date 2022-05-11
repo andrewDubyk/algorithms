06311a08ef08af81d196619cb9c55a60b3851508
andrewDubyk <andrew.dubyk7@gmail.com>
2022-05-11 20:35:34 +0300

    Implement boomerangs counting alogrithm

--- src/boomerangs.rs
+++ src/boomerangs.rs
@@ -1,1 +1,31 @@
+use std::collections::HashMap;
 
+/// Return the number of boomerangs in list of points, where points[i] = (xi, yi)
+/// A boomerang is a tuple of points (i, j, k) such that the distance between i and j equals the
+/// distance between i and k (the order of the tuple matters).
+///
+/// # Arguments
+///
+/// * `points` - Vector of tuples where each tuple contains point coordinates (x,y)
+///
+pub fn count_boomerangs(points: Vec<(i32, i32)>) -> i32 {
+    let mut distance: i32;
+    let mut result: i32 = 0;
+    let mut distance_frequency: HashMap<i32, i32> = HashMap::new();
+    for (i, p1) in points.iter().enumerate() {
+        for (j, p2) in points.iter().enumerate() {
+            if i == j {
+                continue;
+            }
+            distance = (p1.0 - p2.0).pow(2) + (p1.1 - p2.1).pow(2);
+            *distance_frequency.entry(distance).or_default() += 1;
+        }
+
+        for freq in distance_frequency.values() {
+            result += freq * (freq - 1);
+        }
+
+        distance_frequency.clear();
+    }
+    result
+}
--- tests/boomerangs/mod.rs
+++ tests/boomerangs/mod.rs
@@ -1,1 +1,20 @@
+extern crate algorithms;
 
+use algorithms::boomerangs::count_boomerangs;
+use std::vec;
+
+#[test]
+fn it_counts_boomerangs() {
+    {
+        assert_eq!(2, count_boomerangs(vec![(0, 0), (1, 0), (2, 0)]));
+    }
+    {
+        assert_eq!(2, count_boomerangs(vec![(1, 1), (2, 2), (3, 3)]));
+    }
+    {
+        assert_eq!(0, count_boomerangs(vec![(1, 1)]));
+    }
+    {
+        assert_eq!(0, count_boomerangs(vec![]));
+    }
+}
